dc4968d82fb3a27b70852f2dded278704a854d6c
Neujaskre <neujaskre@protonmail.com>
2018-07-22 18:50:02 +0700

    69-87

--- ssrfl/ex2.v
+++ ssrfl/ex2.v
@@ -66,3 +66,74 @@ Proof.
     by [].
 Qed.
 
+Lemma example m p : prime p -> p %| m`! + 1 -> m < p.
+Proof.
+  move=> prime_p.
+  apply contraLR.
+  rewrite -leqNgt => leq_p_m.
+  rewrite dvdn_addr ?dvdn_fact ?prime_gt0 //.
+    by rewrite gtnNdvd // prime_gt1.
+Qed.
+
+Lemma silly_example n : n + 0 = (n + 0) + 0.
+Proof.
+    by rewrite [in RHS] addn0.
+Qed.
+
+Lemma simplify_me : size [:: true] = 1.
+Proof.
+  rewrite //=.
+Qed.
+
+Lemma leq_mul2l' m n1 n2 : (m * n1 <= m * n2) = (m == 0) || (n1 <= n2).
+Proof.
+    by rewrite /leq -mulnBr muln_eq0.
+Qed.
+
+(* Exercises *)
+
+Lemma orbA b1 b2 b3 : b1 || (b2 || b3) = b1 || b2 || b3.
+Proof.
+  case: b1 => //.
+Qed.
+
+Lemma implybE a b : (a ==> b) = ~~ a || b.
+Proof.
+  case: a => //.
+Qed.
+
+Lemma negb_and (a b : bool) : ~~ (a && b) = ~~ a || ~~ b.
+Proof.
+  case: a => //.
+Qed. 
+
+Lemma subn_sqr m n : m ^ 2 - n ^ 2 = (m - n) * (m + n).
+Proof.
+  rewrite -!mulnn.
+Admitted.
+
+Lemma z_muln m : 0 * m = 0.
+Proof. 
+  by [].
+Qed.  
+
+Lemma expn_zero m : (0 ^ m == 0) || (0 ^ m == 1).
+Proof.
+  elim: m => [|m0 IHm] //.
+  rewrite !expnS !z_muln //.
+Qed.
+
+Lemma expn_zero2 m n : 0 ^ m == n = (n == 0) || (n == 1).
+Proof.
+  elim: m => [|m0 IHm].
+  simpl.
+
+
+              
+  elim: n => [|n0 IHn].
+  - simpl.
+  
+Lemma odd_exp m n : odd (m ^ n) = (n == 0) || odd m.
+Proof.
+  elim: m => [|m' IHm].
+  Search "ex
